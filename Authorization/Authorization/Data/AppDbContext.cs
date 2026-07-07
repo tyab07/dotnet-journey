4fc62ce8e8442fd5ffe21f4137f3750ac1888c29
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:36:47 +0500

    auth: add AppDbContext

---
+++ Authorization/Authorization/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Authorization.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Authorization.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option): base(option)
+        {
+             
+        }
+
+
+        public DbSet<User> AccountUser {  get; set; }
+        public DbSet<Employee> Employee { get; set; }
+
+
+    }
+}
