9c5cebc8b68bde381e992b841d666d0b02612418
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:37:27 +0500

    auth: add generic response and migration files

---
+++ Authorization/Authorization/GenericResponse/ResponseResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Authorization.GenericResponse
+{
+    public class ResponseResult<T>
+    {
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+
+        public bool Status { get; set; } = false;
+
+        public static ResponseResult<T> Success(T? data, string message)
+        {   
+            return new ResponseResult<T> { 
+                Data = data,
+                Message = message,
+                Status = true
+            };
+        }
+
+        public static ResponseResult<T> Failure(T? data, string message)
+        {
+            return new ResponseResult<T>
+            {
+                Data = data,
+                Message = message,
+                Status = false
+            };
+        }
+
+
+    }
+}
