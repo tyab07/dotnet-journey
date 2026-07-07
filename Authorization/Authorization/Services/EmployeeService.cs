b08b038689455fdf2e65b2f42bea414e7c8dd8be
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:37:18 +0500

    auth: add EmployeeService

---
+++ Authorization/Authorization/Services/EmployeeService.cs
@@ -0,0 +1,105 @@
+﻿using Authorization.DTOs;
+using Authorization.IServices;
+using Microsoft.EntityFrameworkCore;
+using Authorization.Data;
+
+namespace Authorization.Services
+{
+    public class EmployeeService(AppDbContext _context) : IEmployeeService
+    {
+
+        public async Task<Tuple<int, List<EmployeeDto>>> GetAllEmployeesAsync()
+        {
+            try
+            {
+                return new Tuple<int, List<EmployeeDto>>(1, await _context.Employee.AsNoTracking().Select(x => new EmployeeDto
+                {
+                    Id = x.Id,
+                    CreatedDate = x.CreatedDate,
+                    LastModifiedDate = x.LastModifiedDate,
+                    Name = x.Name,
+                    Dob = x.Dob,
+                    Department = x.Department,
+                    Position = x.Position,
+                    Email = x.Email
+                }).ToListAsync());
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+
+        public async Task<Tuple<int,EmployeeDto,string>> RegisterEmployee(EmployeeDto employeeDto)
+        {
+            try
+            {
+                var existingUser = await _context.Employee.FirstOrDefaultAsync(x => x.Email == employeeDto.Email);
+
+                if (existingUser != null)
+                {
+                    return  new Tuple<int, EmployeeDto, string>(1, null, $"The User{existingUser.Name} is already existing ");
+                }
+
+                _context.Employee.Add(new Entities.Employee
+                {
+                    Name = employeeDto.Name,
+                    CreatedDate = employeeDto.CreatedDate,
+                    Department = employeeDto.Department,
+                    Dob = employeeDto.Dob,
+                    Email = employeeDto.Email,
+                    LastModifiedDate = employeeDto.LastModifiedDate,
+                    Position = employeeDto.Position
+                }
+                );
+
+                await _context.SaveChangesAsync();
+                return new Tuple<int, EmployeeDto, string>(1, employeeDto, "User Successfully register");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+        }
+
+
+
+        public async Task<Tuple<int, EmployeeDto, string>> UpdateEmployee(EmployeeDto employeeDto)
+        {
+            try
+            {
+                var existingUser = await _context.Employee.FirstOrDefaultAsync(x => x.Email == employeeDto.Email);
+
+                if (existingUser == null)
+                {
+                    return new Tuple<int, EmployeeDto, string>(0, null, "The User does not exist ");
+                }
+
+                existingUser.Name = employeeDto.Name ?? existingUser.Name;
+                existingUser.CreatedDate = employeeDto.CreatedDate;
+                existingUser.Department = employeeDto.Department ?? existingUser.Department;
+                existingUser.Dob = employeeDto.Dob ?? existingUser.Dob;
+                existingUser.Email = employeeDto.Email ?? existingUser.Email;
+                existingUser.LastModifiedDate = employeeDto.LastModifiedDate ?? existingUser.LastModifiedDate;
+                existingUser.Position = employeeDto.Position ?? existingUser.Position;
+                
+                await _context.SaveChangesAsync();
+                return new Tuple<int, EmployeeDto, string>(1, employeeDto, "User Successfully Updated");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+        }
+
+
+
+
+    }
+}
