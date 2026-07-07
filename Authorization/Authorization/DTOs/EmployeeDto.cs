724a0425081dad54332cbb0c81433e2c714d828d
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:36:58 +0500

    auth: add DTOs for User and Employee

---
+++ Authorization/Authorization/DTOs/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Authorization.DTOs
+{
+    public class EmployeeDto
+    {
+        public Guid Id { get; set; } 
+
+        public string Name { get; set; } = null!;
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public DateOnly? Dob { set; get; }
+
+        public string? Email { set; get; }
+
+        public string? Position { get; set; }
+
+        public string? Department { get; set; }
+    }
+}
---
+++ Authorization/Authorization/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.DTOs
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
