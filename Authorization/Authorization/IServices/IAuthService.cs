88c2ac173bc82912a4eb537349a5e94d72110afa
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:37:02 +0500

    auth: add service interfaces

---
+++ Authorization/Authorization/IServices/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Authorization.DTOs;
+
+namespace Authorization.IServices
+{
+    public interface IAuthService
+    {
+        Task<Tuple<int, string?>> loginUser(UserDto userDto);
+        Task<Tuple<int, string>> RegisterUser(UserDto userDto);
+       
+
+    }
+}
---
+++ Authorization/Authorization/IServices/IEmployeeService.cs
@@ -0,0 +1,10 @@
+﻿using Authorization.DTOs;
+
+namespace Authorization.IServices
+{
+    public interface IEmployeeService
+    {
+        Task<Tuple<int, List<EmployeeDto>>> GetAllEmployeesAsync();
+        Task<Tuple<int, EmployeeDto, string>> RegisterEmployee(EmployeeDto employeeDto);
+    }
+}
