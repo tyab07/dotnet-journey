965b035a3583d4a4c80e068a5e3cd1d74a3df34f
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:36:54 +0500

    auth: add entities User and Employee

---
+++ Authorization/Authorization/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Authorization.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; } = null!;
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public DateOnly? Dob { set; get; }
+
+        public string? Email { set; get; }
+
+        public string? Position { get; set; }
+
+        public string? Department { get; set; }
+    }
+}
---
+++ Authorization/Authorization/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.Entities
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string? Name { get;set; }
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
