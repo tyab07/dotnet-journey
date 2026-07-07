0066092b15c1f7e24b90c2300ef842f95a5edb35
Tyab07 <tyab.mujahid@gmail.com>
2026-07-07 17:37:23 +0500

    auth: add controllers AuthController and EmployeeController

---
+++ Authorization/Authorization/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Authorization.DTOs;
+using Authorization.GenericResponse;
+using Authorization.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace Authorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly ILogger<AuthController> _logger;
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
+        {
+            _authService = authService;
+            _logger = logger;
+        }
+
+        [HttpPost("login")]
+
+       
+        public async Task<IActionResult> Login(UserDto user)
+        {
+            try
+            {
+             
+                var result = await _authService.loginUser(user);
+                if (result.Item1 == 0)
+                {
+                    return NotFound(ResponseResult<string>.Failure(null, message: result.Item2));
+                }
+                if (result.Item1 == 1)
+                {
+                    return BadRequest(ResponseResult<string>.Failure(null, message : result.Item2));
+                }
+                return Ok(ResponseResult<string>.Success(null, result.Item2));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Registration(UserDto userDto)
+        {
+            try
+            {
+
+                var result = await _authService.RegisterUser(userDto);
+
+                if (result.Item1 == 0)
+                {
+                    return Ok(ResponseResult<string>.Failure(null, message: result.Item2));
+                }
+                return Ok(ResponseResult<string>.Success(null, message: result.Item2));
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
---
+++ Authorization/Authorization/Controllers/EmployeeController.cs
@@ -0,0 +1,47 @@
+﻿using Authorization.DTOs;
+using Authorization.GenericResponse;
+using Authorization.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController(IEmployeeService _employeeService) : ControllerBase
+    {
+        [HttpGet("GetAllEmployees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var result  = await _employeeService.GetAllEmployeesAsync();
+
+            if(!result.Item2.Any())
+            {
+                return Ok(ResponseResult<List<EmployeeDto>>.Failure(null,"No Employee Found!"));
+            }
+
+            return Ok(ResponseResult<List<EmployeeDto>>.Success(result.Item2, "Employee Found!"));
+        }
+
+        [HttpPost("RegisterEmployee")]
+        public async Task<IActionResult> RegisterEmployee(EmployeeDto employeeDto)
+        {
+            var result = await _employeeService.RegisterEmployee(employeeDto);
+            if(result.Item1 == 1 )
+            {
+                return Ok(ResponseResult<EmployeeDto>.Success(result.Item2, result.Item3));
+            }
+            if(result.Item1 == 2)
+            {
+                return Ok(ResponseResult<EmployeeDto>.Success(result.Item2, result.Item3));
+            }
+            return NotFound(ResponseResult<EmployeeDto>.Failure(null, "OOPS! Some thing went wrong..."));
+        }
+
+        //[HttpPatch("UpdateEmployee")]
+        //public async Task<IActionResult> UpdateEmploye(EmployeeDto employeeDto)
+        //{
+
+        //}
+    }
+}
