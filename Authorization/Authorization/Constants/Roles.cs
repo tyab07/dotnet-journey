0ea5da378d85acf95fc284759598740c71dd2c02
Tyab07 <tyab.mujahid@gmail.com>
2026-07-09 12:38:35 +0500

    Create the Roles

---
+++ Authorization/Authorization/Constants/Roles.cs
@@ -0,0 +1,16 @@
+﻿namespace Authorization.Constants
+{
+    public static class Roles
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Admin = "Admin";
+        public const string Employee = "Employee";
+
+        public static readonly HashSet<string> AllRoles = new()
+    {
+        SuperAdmin,
+        Admin,
+        Employee
+    };
+    }
+}
