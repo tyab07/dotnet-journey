fce5e4251b1e05630994159a7f5a19e9d97c7856
Tyab07 <tyab.mujahid@gmail.com>
2026-07-09 12:43:43 +0500

    UserLogin Dto created

---
+++ Authorization/Authorization/DTOs/UserLoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Authorization.DTOs
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
