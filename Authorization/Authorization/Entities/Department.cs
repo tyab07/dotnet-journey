a23a0de2315885b66ea25a823343d8d71652cebb
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:51:22 +0500

    Salary Table Added

--- Authorization/Authorization/Entities/Department.cs
+++ Authorization/Authorization/Entities/Department.cs
@@ -11,5 +11,9 @@ namespace Authorization.Entities
 
         public string HodName { get; set; }
 
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
     }
 }
---
+++ Authorization/Authorization/Entities/Salary.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.Entities
+{
+    public class Salary
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public decimal BasicSalary { get; set; }
+
+        public decimal Bonus { get; set; }
+
+        public decimal Deduction { get; set; }
+
+        public decimal NetSalary { get; set; }
+
+        public DateTime PaymentDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
