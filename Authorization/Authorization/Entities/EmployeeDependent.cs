caad1d43eac2175811c668493fd35594dbb7c5b5
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 16:04:22 +0500

    EmployeeDependant Added

---
+++ Authorization/Authorization/Entities/EmployeeDependent.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Authorization.Entities
+{
+    public class EmployeeDependent
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid EmployeeId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+
+        public string Name { get; set; }
+
+        public string Relationship { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
