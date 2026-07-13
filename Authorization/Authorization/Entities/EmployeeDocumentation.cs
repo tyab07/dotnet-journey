7fb565f841c51a1e08d1f33167ebf7991b71f88e
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:59:25 +0500

    EmployeeDocumentation Table Added

---
+++ Authorization/Authorization/Entities/EmployeeDocumentation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Authorization.Entities
+{
+    public class EmployeeDocumentation
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid EmployeeId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
+
+        public string DocumentName { get; set; }
+
+        public string DocumentNumber { get; set; }
+
+        public string FilePath { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+    }
+}
