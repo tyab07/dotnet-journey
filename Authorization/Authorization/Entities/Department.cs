bb125aba65ba521bbe28083b416f90ee4ee6ea85
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:36:16 +0500

    Department table added

---
+++ Authorization/Authorization/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.Entities
+{
+    public class Department
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string HodName { get; set; }
+
+    }
+}
