ae5bb5f5d872d9472b85bf98006ea94c9f2d98e7
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 16:10:33 +0500

    Salary table Updated

--- Authorization/Authorization/Entities/Salary.cs
+++ Authorization/Authorization/Entities/Salary.cs
@@ -1,14 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Authorization.Entities
 {
     public class Salary
     {
-
         [Key]
         public Guid Id { get; set; }
 
-        public int EmployeeId { get; set; }
+        public Guid EmployeeId { get; set; }
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; }
 
         public decimal BasicSalary { get; set; }
 
