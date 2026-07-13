15d753f2d1de1aa74957fa0bf11a7d50a4119d8f
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:54:58 +0500

    Designation Table Added

---
+++ Authorization/Authorization/Entities/Designation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.Entities
+{
+    public class Designation
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
