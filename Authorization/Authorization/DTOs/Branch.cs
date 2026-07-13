7a8d3e7c33c42f037dc81a222686e61d723315c4
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:55:43 +0500

    Branch Table Added

---
+++ Authorization/Authorization/DTOs/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authorization.Entities
+{
+    public class Branch
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
