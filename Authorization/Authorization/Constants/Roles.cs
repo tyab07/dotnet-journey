a89bb47f4c5ef0908fc3297e45ab7384e9adf356
Tyab07 <tyab.mujahid@gmail.com>
2026-07-13 15:35:00 +0500

    Role updated

--- Authorization/Authorization/Constants/Roles.cs
+++ Authorization/Authorization/Constants/Roles.cs
@@ -1,16 +1,18 @@
-﻿namespace Authorization.Constants
+namespace Authorization.Constants
 {
     public static class Roles
     {
         public const string SuperAdmin = "SuperAdmin";
         public const string Admin = "Admin";
         public const string Employee = "Employee";
+        public const string User = "User";
 
         public static readonly HashSet<string> AllRoles = new()
     {
         SuperAdmin,
         Admin,
-        Employee
+        Employee,
+        User
 
     };
     }
